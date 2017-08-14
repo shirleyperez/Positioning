fcecd61522e60e75756326b65c7cc5ceb64b2490
Shirleyperez <carolinesanchez918@gmail.com>
2017-08-14 10:52:41 -0500

    positioning

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+var contenido = document.getElementsByClassName("contenido")[0];
+var mor = Document.getElementsByClassName("mor");
+var rze = Document.getElementsByClassName("rze");
+var sem = Document.getElementsByClassName("sem");
+var amas = Document.getElementsByClassName("amas");
+
+contenido.style.position = "relative";
+contenido.style.background = "DarkGray";
+contenido.style.width = "600px";
+contenido.style.padding = "50px";
+
+rze.style.position = "absolute";
+rze.style.top = "200px";
+rze.style.right = "0";
+
